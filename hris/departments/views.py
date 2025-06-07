eeb52d74ce01f7b920dd308528c2a620f1580700
TvoiConfig <kolliceplay220@gmail.com>
2025-06-07 16:56:38 +0300

    Добавлены функции поиска и сортировки на странице отделов

--- hris/departments/views.py
+++ hris/departments/views.py
@@ -1,7 +1,31 @@
+from django.db.models import Q
 from django.shortcuts import render
 from .models import Department
 
 # Create your views here.
 def departments_view(request):
+    search_query = request.GET.get('search', '').strip()
+    sort_option = request.GET.get('sort', '')
+
     departments = Department.objects.all()
-    return render(request, 'departments.html', {'departments': departments})
+
+    if search_query:
+        departments = departments.filter(
+            Q(name__icontains=search_query) |
+            Q(desc__icontains=search_query)
+        )
+
+    if sort_option == 'name_asc':
+        departments = departments.order_by('name')
+    elif sort_option == 'name_desc':
+        departments = departments.order_by('-name')
+    elif sort_option == 'created_new':
+        departments = departments.order_by('-created_at')
+    elif sort_option == 'created_old':
+        departments = departments.order_by('created_at')
+
+    return render(request, 'departments.html', {
+        'departments': departments,
+        'search_query': search_query,
+        'sort_option': sort_option
+    })
