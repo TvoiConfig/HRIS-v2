2b3c81bbb2d0dc29bcece05a262887e39edc643f
TvoiConfig <kolliceplay220@gmail.com>
2025-06-08 06:01:25 +0300

    Изменена регистрация модели department

--- hris/departments/admin.py
+++ hris/departments/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
 from departments.models import Department
 
 # Register your models here.
-admin.site.register(Department)
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'desc')
+    ordering = ('name',)
